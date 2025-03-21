6094101356ba7a9688c6f1fc857ba9ad3bafc290
Gishebet Maksim <gishebetmaksim@mail.ru>
2025-03-21 17:56:41 +0300

    Modified Even.java

--- App/src/main/java/hexlet/code/games/Even.java
+++ App/src/main/java/hexlet/code/games/Even.java
@@ -1,15 +1,16 @@
-package hexlet.code;
+package hexlet.code.games;
+
+import hexlet.code.Cli;
+import hexlet.code.App;
 
 import  java.util.Random;
-import java.util.Scanner;
 
 public class Even {
     public static void gameEven() {
-        Scanner scanner = new Scanner(System.in);
+        //Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
         int randInt;
         String answer;
-        //int corrAnswCounter = 0;
 
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
 
@@ -17,13 +18,12 @@ public class Even {
             randInt = rand.nextInt(100);
             System.out.println("Question: " + randInt);
             System.out.print("Your answer: ");
-            answer = scanner.nextLine();
+            answer = App.scanner.next();
             if ((randInt % 2 == 0 && answer.equals("yes")) || (randInt % 2 != 0 && answer.equals("no"))) {
                 System.out.println("Correct!");
-                //corrAnswCounter ++;
             } else {
                 String trueAnswer = answer.equals("yes") ? "no" : "yes";
-                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was " + trueAnswer + ".");
+                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + trueAnswer + "'.");
                 System.out.println("Let's try again, " + Cli.name + "!");
                 return;
             }
