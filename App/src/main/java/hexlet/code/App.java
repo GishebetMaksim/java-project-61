bf6717c20c82761aefc528466f9cc1fb081f09d1
Gishebet Maksim <gishebetmaksim@mail.ru>
2025-03-24 17:23:32 +0300

    added game selection Prime

--- App/src/main/java/hexlet/code/App.java
+++ App/src/main/java/hexlet/code/App.java
@@ -2,10 +2,7 @@ package hexlet.code;
 
 import  java.util.Scanner;
 
-import hexlet.code.games.Calc;
-import hexlet.code.games.Even;
-import hexlet.code.games.GCD;
-import hexlet.code.games.Progression;
+import hexlet.code.games.*;
 
 public class App {
     public static Scanner scanner = new Scanner(System.in);
@@ -17,6 +14,7 @@ public class App {
         System.out.println("3 - Calc");
         System.out.println("4 - GCD");
         System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
         System.out.println("0 - Exit");
 
         System.out.print("Your choice: ");
@@ -25,9 +23,9 @@ public class App {
             int number = scanner.nextInt();
             System.out.println();
 
-            if (number == 0) {
-                scanner.close();
-            } else if (number == 1) {
+//            if (number == 0) {
+//                scanner.close();
+            /*} else*/ if (number == 1) {
                 System.out.println("Welcome to the Brain Games!");
                 Cli.greeting();
             } else if (number == 2) {
@@ -46,6 +44,10 @@ public class App {
                 System.out.println("Welcome to the Brain Games!");
                 Cli.greeting();
                 Progression.getProgression();
+            } else if (number == 6) {
+                System.out.println("Welcome to the Brain Games!");
+                Cli.greeting();
+                Prime.simpleNumber();
             }
         }
     }
